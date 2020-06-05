84077c31e5ddf6a47e408aabd0770700af3a7f3f
Bouchaud <o.bouchaud@etudiant.ecolemelies.fr>
2020-06-05 15:06:01 +0200

    Variables declared;
    > speed;
    > maxSpeed;
    > jumpForce;
    > ground;
    > myRigidbody;
    > stickDirection;
    > myAnimator;

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+[SerializeField] private float speed;
+[SerializeField] private float maxSpeed;
+[SerializeField] private float jumpForce;
+[SerializeField] private LayerMask ground;
+//in order to select one or more of our ground's layers;
+
+private Rigidbody2D myRigidbody2D;
+private Vector2 stickDirection;
+private Animator myAnimator;
+private bool isOnGround = false;
+//to verify if the player is on the ground;
+
     // Start is called before the first frame update
     void Start()
     {
