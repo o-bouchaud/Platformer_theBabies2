cac2776b73cb1ad7d47e243aa70a9165f17e66cf
Kangoo <kangoo.sauvage@gmail.com>
2020-06-08 20:05:21 +0200

    menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+        Debug.Log ("play");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log ("bye bye");
+    }
+}
